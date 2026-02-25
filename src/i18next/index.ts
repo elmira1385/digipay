1d60dbb182946b55589dbfec2247e5f1e842ec84
elmira1385 <elmirashokriyan64@gmail.com>
2026-02-25 16:58:44 +0330

    add i18next

---
+++ src/i18next/index.ts
@@ -0,0 +1,27 @@
+// i18n.js
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    fa: {
+      translation: {
+        hello: "سلام دنیا",
+        welcome: "به سایت من خوش آمدی"
+      }
+    },
+    en: {
+      translation: {
+        hello: "Hello World",
+        welcome: "Welcome to my website"
+      }
+    }
+  },
+  lng: "fa",
+  fallbackLng: "fa",
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+export default i18n;
